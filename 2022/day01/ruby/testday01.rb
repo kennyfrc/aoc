362bbafdbaebe8eba15656b4e7585e75e753cb40
kennyfrc <fxkennyfrc@gmail.com>
2022-12-01 20:45:38 +0900

    test: fix function nameg

--- 2022/day01/ruby/testday01.rb
+++ 2022/day01/ruby/testday01.rb
@@ -6,8 +6,8 @@ require_relative 'day01'
 # Test for max_calories function
 class TestMaxCalories < Minitest::Test
   def test_max_calories
-    assert_equal 24_000, get_max_calories('example.txt')
-    assert_equal 400, get_max_calories('example2.txt')
-    assert_equal 70_698, get_max_calories('input.txt')
+    assert_equal 24_000, max_calories('example.txt')
+    assert_equal 400, max_calories('example2.txt')
+    assert_equal 70_698, max_calories('input.txt')
   end
 end
