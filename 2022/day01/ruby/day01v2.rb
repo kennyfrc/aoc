29719a799f554d29b5c33451ccea0da9f853b84f
kennyfrc <fxkennyfrc@gmail.com>
2022-12-05 09:52:14 +0800

    day01v2: refactored

--- 2022/day01/ruby/day01v2.rb
+++ 2022/day01/ruby/day01v2.rb
@@ -1,72 +1,76 @@
 # frozen_string_literal: true
 
-class Elf
-  attr_reader :food
+module MaxCaloriesv2
+  class Elf
+    attr_reader :food
 
-  def initialize
-    @food = []
-  end
+    def initialize
+      @food = []
+    end
 
-  def add(dish)
-    food << dish
-  end
+    def add(dish)
+      food << dish
+    end
 
-  def sum
-    food.sum
-  end
+    def sum
+      food.sum
+    end
 
-  def merge(blk)
-    _blk = Elf.new
-    _blk.add(sum + blk.sum)
-    _blk
-  end
-end
-
-def sort_elves!(elves, current_elf)
-  if current_elf.sum > elves[0].sum
-    elves[2] = elves[1]
-    elves[1] = elves[0]
-    elves[0] = current_elf
-  elsif current_elf.sum > elves[1].sum
-    elves[2] = elves[1]
-    elves[1] = current_elf
-  elsif current_elf.sum > elves[2].sum
-    elves[2] = current_elf
-  end
-  elves
-end
-
-def organize_elves!(file:, elves:, current_elf:)
-  File.foreach(file) do |line|
-    if line != "\n"
-      line.to_i
-      current_elf.add(line.to_i)
-    elsif line == "\n"
-      case elves.size
-      when 0..2
-        elves << current_elf
-      end
-      sort_elves!(elves, current_elf) if elves.size == 3
-      current_elf = Elf.new
-    else
-      raise StandardError, 'Invalid Format. Each integer must be separated with newlines.'
+    def merge(blk)
+      _blk = Elf.new
+      _blk.add(sum + blk.sum)
+      _blk
     end
   end
-  [elves, current_elf]
-end
 
-def run(file, flag)
-  elves, current_elf = organize_elves!(file: file, elves: [], current_elf: Elf.new)
-  sort_elves!(elves, current_elf) # integer in the last line of the file
-  return elves.map.reduce(:merge).sum if flag == 3
-  return elves[0].sum if flag == 1
+  def self.sort_elves(elves, current_elf)
+    elves = elves.clone
+    if current_elf.sum > elves[0].sum
+      elves[2] = elves[1]
+      elves[1] = elves[0]
+      elves[0] = current_elf
+    elsif current_elf.sum > elves[1].sum
+      elves[2] = elves[1]
+      elves[1] = current_elf
+    elsif current_elf.sum > elves[2].sum
+      elves[2] = current_elf
+    end
+    elves
+  end
+
+  def self.organize_elves(file:, elves:, current_elf:)
+    current_elf = current_elf.clone
+    elves = elves.clone
+    File.foreach(file) do |line|
+      if line != "\n"
+        line.to_i
+        current_elf.add(line.to_i)
+      elsif line == "\n"
+        case elves.size
+        when 0..2
+          elves << current_elf
+        end
+        elves = sort_elves(elves, current_elf) if elves.size == 3
+        current_elf = Elf.new
+      else
+        raise StandardError, 'Invalid Format. Each integer must be separated with newlines.'
+      end
+    end
+    [elves, current_elf]
+  end
+
+  def self.max_calories(file, flag)
+    elves, current_elf = organize_elves(file: file, elves: [], current_elf: Elf.new)
+    elves = sort_elves(elves, current_elf) # integer in the last line of the file
+    return elves.map.reduce(:merge).sum if flag == 3
+    return elves[0].sum if flag == 1
+  end
 end
 
 if ARGV.empty?
   puts 'Usage: ruby day01.rb <file> <flag={1,3}>'
-  exit
 else
   file = ARGV[0]
   flag = (ARGV[1] || 1).to_i # default to 1
-  p run(file, flag)
+  p MaxCaloriesv2::max_calories(file, flag)
 end
--- 2022/day01/ruby/testday01.rb
+++ 2022/day01/ruby/testday01.rb
@@ -2,6 +2,7 @@
 
 require 'minitest/autorun'
 require_relative 'day01'
+require_relative 'day01v2'
 
 # Test for max_calories function
 class TestMaxCalories < Minitest::Test
@@ -9,10 +10,15 @@ class TestMaxCalories < Minitest::Test
     assert_equal 24_000, max_calories('example.txt')
     assert_equal 400, max_calories('example2.txt')
     assert_equal 70_698, max_calories('input.txt')
+    assert_equal 24_000, MaxCaloriesv2::max_calories('example.txt', 1)
+    assert_equal 400, MaxCaloriesv2::max_calories('example2.txt', 1)
+    assert_equal 70_698, MaxCaloriesv2::max_calories('input.txt', 1)
   end
 
   def test_total_of_top3
     assert_equal 45_000, total_calories_of_top3('example.txt')
     assert_equal 699, total_calories_of_top3('example2.txt')
+    assert_equal 45_000, MaxCaloriesv2::max_calories('example.txt', 3)
+    assert_equal 699, MaxCaloriesv2::max_calories('example2.txt', 3)
   end
 end
